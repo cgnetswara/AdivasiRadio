bf78ede8bf9bebb46da98848140baa22a74934b2
Anurag Shukla <shukla.anurag0006@gmail.com>
2019-07-25 22:19:36 +0530

    Navigations added: BookReadActivity

--- app/src/main/java/com/example/myapplication/BookRead.java
+++ app/src/main/java/com/example/myapplication/BookRead.java
@@ -3,16 +3,28 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.yuyakaido.android.cardstackview.CardStackLayoutManager;
+import com.yuyakaido.android.cardstackview.CardStackListener;
 import com.yuyakaido.android.cardstackview.CardStackView;
 import com.yuyakaido.android.cardstackview.Direction;
 
 import java.util.ArrayList;
 
+import static com.example.myapplication.Dashboard.dashboard_activity;
+
 public class BookRead extends AppCompatActivity {
 
     CardStackView bookPageStackView;
+    FloatingActionButton forwardButtonFloating;
+    FloatingActionButton backwardButtonFloating;
+    FloatingActionButton ttsButtonImageView;
+    Boolean TTS_On = false;
+    static int currentPagePos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +32,86 @@ public class BookRead extends AppCompatActivity {
         setContentView(R.layout.activity_book_read);
 
         bookPageStackView = findViewById(R.id.bookStackView);
-        CardStackLayoutManager cardStackLayoutManager =  new CardStackLayoutManager(this);
+        forwardButtonFloating = findViewById(R.id.forwardArrow);
+        backwardButtonFloating = findViewById(R.id.backwardArrow);
+        ttsButtonImageView = findViewById(R.id.ttsButton);
+
+        final ArrayList<BookPage> bookPages = DataUtils.getBook();
+
+        forwardButtonFloating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bookPageStackView.swipe();
+            }
+        });
+
+        backwardButtonFloating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bookPageStackView.rewind();
+            }
+        });
+
+        ttsButtonImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!TTS_On) {
+                    dashboard_activity.sayText(bookPages.get(currentPagePos).getBookPageText(), TextToSpeech.QUEUE_FLUSH);
+                    ttsButtonImageView.setImageResource(R.drawable.tts_pause_icon);
+                    TTS_On = true;
+                } else{
+                    ttsButtonImageView.setImageResource(R.drawable.tts_icon);
+                    dashboard_activity.stopSpeaking();
+                    TTS_On = false;
+                }
+            }
+        });
+
+        CardStackListener listener = new CardStackListener() {
+            @Override
+            public void onCardDragging(Direction direction, float ratio) {
+
+            }
+
+            @Override
+            public void onCardSwiped(Direction direction) {
+
+            }
+
+            @Override
+            public void onCardRewound() {
+
+            }
+
+            @Override
+            public void onCardCanceled() {
+
+            }
+
+            @Override
+            public void onCardAppeared(View view, int position) {
+
+                currentPagePos = position;
+
+            }
+
+            @Override
+            public void onCardDisappeared(View view, int position) {
+                dashboard_activity.stopSpeaking();
+                ttsButtonImageView.setImageResource(R.drawable.tts_icon);
+                TTS_On = false;
+
+            }
+        };
+
+        CardStackLayoutManager cardStackLayoutManager =  new CardStackLayoutManager(this, listener);
         cardStackLayoutManager.setCanScrollHorizontal(false);
         cardStackLayoutManager.setDirections(Direction.VERTICAL);
         cardStackLayoutManager.setCanScrollVertical(true);
 
         bookPageStackView.setLayoutManager(cardStackLayoutManager);
 
-        ArrayList<BookPage> bookPages = DataUtils.getBook();
+        bookPageStackView = findViewById(R.id.bookStackView);
 
 
         bookPageStackView.setAdapter(new BookPageAdapter(bookPages));
